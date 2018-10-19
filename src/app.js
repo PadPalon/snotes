06ee7c44e9f5c16995a172cc19f37877bff8cdc2
Luca Neukom <luca.neukom@gmail.com>
2018-10-19 23:24:04 +0200

    build basic data binding

--- src/app.js
+++ src/app.js
@@ -1,27 +1,71 @@
-new Vue({
+let noteRow = {
+    props: ["note"],
+    template: `
+        <tr>
+            <td>{{ note.timestamp }}</td>
+            <td>{{ note.text }}</td>
+            <td>{{ note.labels }}</td>
+            <td class="icon_column"><i v-on:click="$parent.$emit('note-removed', note)" class="fas fa-trash-alt"></i></td>
+        </tr>`
+};
+
+let app = new Vue({
     el: '#vue-container',
     data: {
         songs: [],
         notes: [],
+        currentSong: {
+            id: 0,
+            title: "",
+            genre: ""
+        },
+        songCount: 1,
+        noteCount: 1,
         list: true
     },
     methods: {
-        createSong: function() {
-            this.songs.push({
-                title: "title",
-                genre: "genre"
-            })
+        newSong: function() {
+            this.list = false;
+            this.currentSong = {
+                id: 0,
+                title: "",
+                genre: ""
+            }
         },
-        createNote: function() {
+        saveSong: function () {
+            if(this.currentSong.id === 0) {
+                this.currentSong.id = this.songCount;
+                this.songs.push(this.currentSong);
+                this.songCount++;
+            } else {
+                let song = this.songs.find(e => e.id === this.currentSong.id);
+                song.title = this.currentSong.title;
+                song.genre = this.currentSong.genre;
+            }
+        },
+        editSong: function (song) {
+            this.list = false;
+            this.currentSong = song;
+        },
+        createNote: function () {
             this.notes.push({
-                timestamp: "Time",
-                text: "Text",
-                labels: "Label, Label",
-            })
-        },
-        deleteNote: function(index) {
-            this.notes.splice(index, 1);
+                id: this.noteCount,
+                timestamp: "",
+                text: "",
+                labels: "",
+            });
+            this.noteCount++;
         }
+    },
+    components: {
+        "note-row": noteRow
     }
 });
 
+app.$on("note-removed", function(note) {
+   app.notes.splice(app.notes.indexOf(note), 1);
+});
+
+app.$on("song-removed", function(song) {
+   app.songs.splice(app.songs.indexOf(song), 1);
+});
